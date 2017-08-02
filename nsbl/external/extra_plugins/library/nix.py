d6054b6d5efdfff87eee47c166e118c7a14fd2ec
Markus Binsteiner <makkus@posteo.de>
2017-08-02 20:23:31 +0700

    Enabled nix.py for multi-user nix.

--- nsbl/external/extra_plugins/library/nix.py
+++ nsbl/external/extra_plugins/library/nix.py
@@ -19,34 +19,61 @@ from distutils.spawn import find_executable
 from ansible.module_utils.basic import *
 
 NIX_PATH = os.path.join(os.environ['HOME'], ".nix-profile")
-NIX_SOURCE_PATH = os.path.join(NIX_PATH, 'etc', 'profile.d', 'nix.sh')
-NIX_ENV_PATH = os.path.join(NIX_PATH, "bin", "nix-env")
-NIX_CHANNEL_PATH = os.path.join(NIX_PATH, "bin", "nix-channel")
+SINGLE_USER_NIX_PATH = os.path.join(NIX_PATH, "bin")
+MULTI_USER_MAC_NIX_PATH = "/nix/var/nix/profiles/default/bin"
+
+POTENTIAL_NIX_PATHS = [SINGLE_USER_NIX_PATH, MULTI_USER_MAC_NIX_PATH]
+
+SINGLE_USER_SOURCE_PATH = os.path.join(NIX_PATH, 'etc', "profile.d", "nix.sh")
+MULTI_USER_SOURCE_PATH = "/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh"
+POTENTIAL_SOURCE_FILES = [SINGLE_USER_SOURCE_PATH, MULTI_USER_SOURCE_PATH]
+
+# NIX_SOURCE_PATH = os.path.join(NIX_PATH, 'etc', 'profile.d', 'nix.sh')
+# NIX_ENV_PATH = os.path.join(NIX_PATH, "bin", "nix-env")
+# NIX_CHANNEL_PATH = os.path.join(NIX_PATH, "bin", "nix-channel")
 
 # this creates the nix env in case the run was started without that environment set
 WRAP = True
 
+BIN_CACHE = {}
+
+def get_source_file(module):
+
+    for source_file in POTENTIAL_SOURCE_FILES:
+        if os.path.exists(source_file):
+            return source_file
+
+    module.fail_json(msg="Could not find nix source environment.")
+
 def get_nix_bin(module, bin_name):
 
-    conda_bin = find_executable('conda')
-    if not conda_bin:
-        for path in POTENTIAL_CONDA_PATHS:
-            if os.path.isfile(os.path.join(path, 'conda')):
-                conda_bin = os.path.join(path, 'conda')
+    nix_bin = BIN_CACHE.get(bin_name, False)
 
-    if not conda_bin:
-        module.fail_json(msg="Could not find conda environment.")
+    if not nix_bin:
 
-    return conda_bin
+        nix_bin = find_executable(bin_name)
+        if not nix_bin:
+            tried = []
+            for path in POTENTIAL_NIX_PATHS:
+                tried.append(path)
+                if os.path.isfile(os.path.join(path, bin_name)):
+                    nix_bin = os.path.join(path, bin_name)
+
+        if not nix_bin:
+            module.fail_json(msg="Could not find nix environment for executable '{}', tried: {}".format(bin_name, tried))
+
+        BIN_CACHE[bin_name] = nix_bin
+
+    return nix_bin
 
 
 
 def query_package(module, name, state="present"):
     if state == "present":
         if WRAP:
-            cmd = 'bash -c "source {}; {} -q {}"'.format(NIX_SOURCE_PATH, NIX_ENV_PATH, name)
+            cmd = 'bash -c "source {}; {} -q {}"'.format(get_source_file(module), get_nix_bin(module, "nix-env"), name)
         else:
-            cmd = "{} -q {}".format(NIX_ENV_PATH, name)
+            cmd = "{} -q {}".format(get_nix_bin(module, "nix-env"), name)
 
 
         rc, stdout, stderr = module.run_command(cmd, check_rc=False)
@@ -59,9 +86,9 @@ def query_package(module, name, state="present"):
 def update_cache(module):
 
     if WRAP:
-        cmd = 'bash -c "source {}; {} --update"'.format(NIX_SOURCE_PATH, NIX_CHANNEL_PATH)
+        cmd = 'bash -c "source {}; {} --update"'.format(get_source_file(module), get_nix_bin(module, "nix-channel"))
     else:
-        cmd = "{} --update".format(NIX_CHANNEL_PATH)
+        cmd = "{} --update".format(get_nix_bin(module, "nix-channel"))
 
     rc, stdout, stderr = module.run_command(cmd, check_rc=False)
     if rc != 0:
@@ -72,9 +99,9 @@ def update_cache(module):
 def upgrade_packages(module):
 
     if WRAP:
-        cmd = 'bash -c "source {}; {} --upgrade"'.format(NIX_SOURCE_PATH, NIX_ENV_PATH)
+        cmd = 'bash -c "source {}; {} --upgrade"'.format(get_source_file(module), get_nix_bin(module, "nix-env"))
     else:
-        cmd = "{} --upgrade".format(NIX_ENV_PATH)
+        cmd = "{} --upgrade".format(get_nix_bin(module, "nix-env"))
 
     rc, stdout, stderr = module.run_command(cmd, check_rc=False)
     if rc != 0:
@@ -99,9 +126,9 @@ def install_packages(module, packages):
             continue
 
         if WRAP:
-            cmd = 'bash -c "source {}; {} -i {}"'.format(NIX_SOURCE_PATH, NIX_ENV_PATH, package)
+            cmd = 'bash -c "source {}; {} -i {}"'.format(get_source_file(module), get_nix_bin(module, "nix-env"), package)
         else:
-            cmd = "{} -i {}".format(NIX_ENV_PATH, package)
+            cmd = "{} -i {}".format(get_nix_bin(module, "nix-env"), package)
 
         rc, stdout, stderr = module.run_command(cmd, check_rc=False)
 
@@ -126,8 +153,8 @@ def main():
         mutually_exclusive=[['name', 'upgrade']],
         supports_check_mode=True)
 
-    if not os.path.exists(NIX_ENV_PATH):
-        module.fail_json(msg="cannot find nix-env, looking for %s" % (NIX_ENV_PATH))
+    if not os.path.exists(get_nix_bin(module, "nix-env")):
+        module.fail_json(msg="cannot find nix-env, looking for %s" % (get_nix_bin(module, "nix-env")))
 
     p = module.params
 
