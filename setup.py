be96ba55e5e443dc89293e66097054415401a5bd
Markus Binsteiner <makkus@posteo.de>
2017-08-03 15:56:01 +0700

    Bump version: 0.0.28 → 0.0.29

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='nsbl',
-    version='0.0.28',
+    version='0.0.29',
     description="elastic ansible configuration",
     long_description=readme + '\n\n' + history,
     author="Markus Binsteiner",
