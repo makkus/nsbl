259f14d9f60c58d23b41773db42f3b30d07b335f
Markus Binsteiner <makkus@posteo.de>
2018-02-01 14:57:55 +0700

    Bump version: 0.3.3 → 0.3.4

--- nsbl/__init__.py
+++ nsbl/__init__.py
@@ -5,4 +5,4 @@
 
 __author__ = """Markus Binsteiner"""
 __email__ = 'makkus@posteo.de'
-__version__ = '0.3.3'
+__version__ = '0.3.4'
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='nsbl',
-    version='0.3.3',
+    version='0.3.4',
     description="elastic ansible configuration",
     long_description=readme + '\n\n' + history,
     author="Markus Binsteiner",
